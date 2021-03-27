14928b83b8a22ec1dd419a6970b5762d3135e109
Kyle Euman <euman_kyle@yahoo.com>
2021-03-27 14:33:39 -0400

    started adding questions

--- index.js
+++ index.js
@@ -4,8 +4,64 @@ const util = require("util");
 const inquirer = require("inquirer");
 const generateReadme = require("./utils/generateReadme")
 const writeFileAsync = util.promisify(fs.writeFile);
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+ {          type: 'input',
+            name: 'Project Title',
+            message: 'What is the Project Title',
+}
+
+{          type: 'input'
+           name: 
+           message: 
+}
+
+{          type: 'input'
+           name: 
+           message: 
+}
+
+{          type: 'input'
+           name: 
+           message: 
+}
+
+{          type: 'list'
+           name: 
+           message: 
+}
+
+{          type: 'input'
+           name: 
+           message: 
+}
+
+{          type: 'input'
+           name: 
+           message: 
+}
+
+{          type: 'input'
+           name: 
+           message: 
+}
+
+{          type: 'input'
+           name: 
+           message: 
+}
+
+{          type: 'input'
+           name: 
+           message: 
+}
+
+{          type: 'input'
+           name: 
+           message: 
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
