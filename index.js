53793e6a38f74a053b8901e1116958daa4e43a48
Kyle Euman <euman_kyle@yahoo.com>
2021-03-27 14:54:20 -0400

    made some changes to json package file

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
-const generateReadme = require("./utils/generateReadme")
-const writeFileAsync = util.promisify(fs.writeFile);
+
 
 // TODO: Create an array of questions for user input
 const questions = [
